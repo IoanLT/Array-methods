bf1f35c1819a7bd5081b0d07a0e9f17c76fbffef
IoanLT <ioan.tranole@gmail.com>
2020-10-28 16:55:54 +0000

    Testing the map method

--- script.js
+++ script.js
@@ -21,11 +21,32 @@
 // console.log(premiumUsers);
 
 
+// Map method
 
+// const prices = [20, 10, 30, 25, 15, 40, 80, 5];
 
+// const salePrices = prices.map(price => price / 2);
 
+// console.log(salePrices);
 
+const products = [
+    {name: 'star', price: 20},
+    {name: 'mushroom', price: 40},
+    {name: 'shells', price: 30},
+    {name: 'banana', price: 10},
+    {name: 'something', price: 50},
+];
 
+const saleProducts = products.map(product => {
+    if(product.price > 30){
+        return {name: product.name, price: product.price / 2};
+    } else {
+        return product;
+    }
+});
+
+console.log(products);
+console.log(saleProducts);
 
 
 
