ee4b3413c91e8cebc91b9cc2e2de99ba4a3a604b
IoanLT <ioan.tranole@gmail.com>
2020-10-28 15:25:22 +0000

    Testing the filter method

---
+++ script.js
@@ -0,0 +1,47 @@
+// Filter method
+
+// const scores = [10, 20, 15, 25, 50, 40, 5];
+
+// const scoresFiltered = scores.filter(score => score > 20);
+// console.log(scores);
+// console.log(scoresFiltered);
+
+// const users = [
+//     {name: 'nelu', premium: true},
+//     {name: 'yoshi', premium: false},
+//     {name: 'mario', premium: false},
+//     {name: 'luigi', premium: true}
+// ];
+
+// const premiumUsers = users.filter(user => {
+//     return user.premium;
+// })
+
+// console.log(users);
+// console.log(premiumUsers);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
