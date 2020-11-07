29f96fa1774ed727c2bcf80852aea4ece299db7d
IoanLT <ioan.tranole@gmail.com>
2020-11-07 15:21:57 +0000

    Completing advance arrays exercise WCS

--- script.js
+++ script.js
@@ -56,6 +56,89 @@
 
 
 
+// Exercise 1 - Wild Code School
+const instructors = [
+    {
+      name: 'John',
+      availabilty: 'all',
+      specialities: ['Javascript', 'Python', 'C++']
+    },
+    {
+      name: 'Mary',
+      availabilty: 'weekend',
+      specialities: ['Javascript', 'Ruby', 'C++']
+    },
+    {
+      name: 'Chris',
+      availabilty: 'evenings',
+      specialities: ['Javascript']
+    },
+    {
+      name: 'Anthony',
+      availabilty: 'all',
+      specialities: ['Python', 'Ruby']
+    },
+    {
+      name: 'Pauline',
+      availabilty: 'only Mondays',
+      specialities: ['Javascript', 'Html', 'CSS']
+    },
+    {
+      name: 'Mark',
+      availabilty: 'all',
+      specialities: ['C#', 'C++', 'Javascript']
+    },
+    {
+      name: 'Helen',
+      availabilty: 'evenings',
+      specialities: ['Python', 'C++']
+    },
+    {
+      name: 'Charles',
+      availabilty: 'none',
+      specialities: ['Python']
+    }
+  ];
+  
+  let codeInstructors = instructors.filter((instructor) => {  
+    if(instructor.specialities.includes('Javascript') && (instructor.availabilty === 'all' || instructor.availabilty === 'weekend')) {
+      return instructor.name;
+    } 
+  });
+  console.log(codeInstructors);
+  
+  // PART 2
+  // Iterate over that new array of instructors available and show a message per instructor saying:
+  // Hi nameOfInstructor, we inform you that this weekend you will be doing the support class
+  
+  codeInstructors.forEach(instructor => {
+    console.log(`Hi ${instructor.name}, we inform you that this weekend you will be doing the support class`);
+  });
+  
+  // PART 3
+  // Modify the previous message checking that if an instructor also knows about Python, the message needs to be:
+  // Hi nameOfInstructor, we inform you that this weekend you will be doing the support class and you need to prepare a Python workshop
+  
+instructors.forEach(instructor => {
+    if((instructor.specialities.includes('Python') && instructor.specialities.includes('Javascript')) 
+    && (instructor.availabilty === 'all' || instructor.availabilty === 'weekend')) {
+        console.log(`Hi ${instructor.name}, we inform you that this weekend you will be doing the support class and you need to prepare a Python workshop`);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
