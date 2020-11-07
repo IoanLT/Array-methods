383b357d9b380a88dc957358ae3408dd1063d0e9
IoanLT <ioan.tranole@gmail.com>
2020-11-07 13:51:25 +0000

    Completed the Advanced arrays challenge from WCS

--- script.js
+++ script.js
@@ -29,20 +29,88 @@
 
 
 // Testing the reduce method
-const cart = [
-    { name: "eggs", price: 2 },
-    { name: "milk", price: 1 },
-    { name: "tomatoes", price: 1 },
-    { name: "Cheese", price: 2 }
-];
+// const cart = [
+//     { name: "eggs", price: 2 },
+//     { name: "milk", price: 1 },
+//     { name: "tomatoes", price: 1 },
+//     { name: "Cheese", price: 2 }
+// ];
   
-let result = cart.reduce((acc, current) => {    
-    return acc.price + current.price; 
+// let result = cart.reduce((acc, current) => {    
+//     return acc.price + current.price; 
+// });
+// console.log(result);
+
+
+// Exercise 1
+const instructors = [
+    {
+      name: 'John',
+      availabilty: 'all',
+      specialities: ['Javascript', 'Python', 'C++']
+    },
+    {
+      name: 'Mary',
+      availabilty: 'weekend',
+      specialities: ['Javascript', 'Ruby', 'C++']
+    },
+    {
+      name: 'Chris',
+      availabilty: 'evenings',
+      specialities: ['Javascript']
+    },
+    {
+      name: 'Anthony',
+      availabilty: 'all',
+      specialities: ['Python', 'Ruby']
+    },
+    {
+      name: 'Pauline',
+      availabilty: 'only Mondays',
+      specialities: ['Javascript', 'Html', 'CSS']
+    },
+    {
+      name: 'Mark',
+      availabilty: 'all',
+      specialities: ['C#', 'C++', 'Javascript']
+    },
+    {
+      name: 'Helen',
+      availabilty: 'evenings',
+      specialities: ['Python', 'C++']
+    },
+    {
+      name: 'Charles',
+      availabilty: 'none',
+      specialities: ['Python']
+    }
+  ];
+  
+  let codeInstructors = instructors.filter((instructor) => {  
+    if(instructor.specialities.includes('Javascript') && (instructor.availabilty === 'all' || instructor.availabilty === 'weekend')) {
+      return instructor.name;
+    } 
+  });
+  console.log(codeInstructors);
+  
+  // PART 2
+  // Iterate over that new array of instructors available and show a message per instructor saying:
+  // Hi nameOfInstructor, we inform you that this weekend you will be doing the support class
+  
+  codeInstructors.forEach(instructor => {
+    console.log(`Hi ${instructor.name}, we inform you that this weekend you will be doing the support class`);
+  });
+  
+  // PART 3
+  // Modify the previous message checking that if an instructor also knows about Python, the message needs to be:
+  // Hi nameOfInstructor, we inform you that this weekend you will be doing the support class and you need to prepare a Python workshop
+  
+instructors.forEach(instructor => {
+    if((instructor.specialities.includes('Python') && instructor.specialities.includes('Javascript')) 
+    && (instructor.availabilty === 'all' || instructor.availabilty === 'weekend')) {
+        console.log(`Hi ${instructor.name}, we inform you that this weekend you will be doing the support class and you need to prepare a Python workshop`);
+    }
 });
-console.log(result);
-
-
-
 
 
 
